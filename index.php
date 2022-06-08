6fb612dcd5634c8e91400ba5ffa748f27687f163
eber <louistone983@gmail.com>
2022-06-08 17:17:06 +0800

    no message

--- index.php
+++ index.php
@@ -1,27 +1,40 @@
-<div class="ImgBack"></div>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ChatTalker</title>
 
-<div class="container">
-    <div class="nav">
-        <ul>
-            <li class='logo'>
-                <a href="#"><img src="https://github.com/hexschool/webLayoutTraining1st/blob/master/chatTalker_images/logo.png?raw=true" alt=""></a>
-            </li>
-            <li class='price'>
-                <a href="#"><span>方案費用</span></a>
-            </li>
-        </ul>
-    </div>
-    <div class="banner">
-        <div class="slogan">
-            <h1>ChatTalker 最全面的聊天機器人</h1>
-            <h2>你愛我像誰？<br>扮演什麼角色我都會</h2>
-            <h3>Facebook、Instagram、LINE 聊天機器人解決方案</h3>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="index.css"/>
+</head>
+<body>
+    <div class="ImgBack"></div>
+
+    <div class="container">
+        <div class="nav">
+            <ul>
+                <li class='logo'>
+                    <a href="#"><img src="https://github.com/hexschool/webLayoutTraining1st/blob/master/chatTalker_images/logo.png?raw=true" alt=""></a>
+                </li>
+                <li class='price'>
+                    <a href="#"><span>方案費用</span></a>
+                </li>
+            </ul>
         </div>
-        <div class="bannerImg">
-            <div class="bannerImgLike"></div>
-            <div class="bannerImgBack">
-                <div class="bannerTop"><span>Top</span></div>
+        <div class="banner">
+            <div class="slogan">
+                <h1>ChatTalker 最全面的聊天機器人</h1>
+                <h2>你愛我像誰？<br>扮演什麼角色我都會</h2>
+                <h3>Facebook、Instagram、LINE 聊天機器人解決方案</h3>
+            </div>
+            <div class="bannerImg">
+                <div class="bannerImgLike"></div>
+                <div class="bannerImgBack">
+                    <div class="bannerTop"><span>Top</span></div>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</body>
+</html>
+
