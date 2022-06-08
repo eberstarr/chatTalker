1c308d2f5a3a11fb254644942bceabf1c8e01dfe
eber <louistone983@gmail.com>
2022-06-08 17:13:02 +0800

    no message

---
+++ index.php
@@ -0,0 +1,27 @@
+<div class="ImgBack"></div>
+
+<div class="container">
+    <div class="nav">
+        <ul>
+            <li class='logo'>
+                <a href="#"><img src="https://github.com/hexschool/webLayoutTraining1st/blob/master/chatTalker_images/logo.png?raw=true" alt=""></a>
+            </li>
+            <li class='price'>
+                <a href="#"><span>方案費用</span></a>
+            </li>
+        </ul>
+    </div>
+    <div class="banner">
+        <div class="slogan">
+            <h1>ChatTalker 最全面的聊天機器人</h1>
+            <h2>你愛我像誰？<br>扮演什麼角色我都會</h2>
+            <h3>Facebook、Instagram、LINE 聊天機器人解決方案</h3>
+        </div>
+        <div class="bannerImg">
+            <div class="bannerImgLike"></div>
+            <div class="bannerImgBack">
+                <div class="bannerTop"><span>Top</span></div>
+            </div>
+        </div>
+    </div>
+</div>
